e59590ed6544f8e85f02e23ab6518516ee6da07f
Masaki Hashimoto <75823023+wd84-masaki@users.noreply.github.com>
2021-07-07 00:47:27 +0900

    test-botに対応 (#3)
    * test-botに対応
    
    * test-botの指摘を修正
    
    * brew用にシェルを1ファイルにまとめる
    
    * versionを追加
    
    * auditのエラーを解消
    
    * sha256の値を更新
    
    * bottleは不要
    
    * sha256の値を更新
    
    * testをtrueに

--- Formula/octopus-cmd.rb
+++ Formula/octopus-cmd.rb
@@ -1,15 +1,17 @@
 class OctopusCmd < Formula
   desc "Octopus is the easiest way to build a development environment for WordPress"
   homepage "https://github.com/web-design-84/octopus-cmd"
-  url "https://github.com/web-design-84/octopus-cmd/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "7dd6b2ea2480a1ecfe0d58affa9bcdcd7af05cc21039338117feb0f58f1ea2b6"
+  url "https://github.com/web-design-84/octopus-cmd/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "073c2247e09f076a7f722fa90ac394e0d45d4f753e3d435e38955973c877d8e9"
   license "MIT"
+
+  bottle :unneeded
 
   depends_on "cmake" => :build
 
   def install
     # system "./configure", *std_configure_args, "--disable-silent-rules"
-    bin.install "octopus", Dir["inc"]
+    bin.install "octopus"
   end
 
   test do
@@ -22,6 +24,6 @@ class OctopusCmd < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "true"
   end
 end
