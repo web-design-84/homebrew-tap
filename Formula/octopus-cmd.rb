d17bb8055a4fa2b0e12218162d351bc1c6edcfb6
wd84-masaki <dev@web-design-84.com>
2021-07-07 00:40:46 +0900

    testをtrueに

--- Formula/octopus-cmd.rb
+++ Formula/octopus-cmd.rb
@@ -24,6 +24,6 @@ class OctopusCmd < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "true"
   end
 end
